a4c9bacdc735bbafde936cb85262160df0f693f5
faryadazim <faryadmaalic92@yahoo.com>
2022-02-04 14:17:48 +0500

    rebuiled

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import Posts from './Posts/Posts.js'
+import Form from './Form/Form.js'
 const App = () => {
   return <>
-  <h2>Hello</h2>
+  <h3 className='text-center my-4' >tomHeart </h3>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-8"><Posts/></div>
+      <div className="col-md-4"><Form/></div>
+    </div>
+  </div>
   
   </>;
 };
---
+++ client/src/Posts/Posts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Post from './Post/Post.js'
+const Posts = () => {
+  return (
+    <div>
+      <Post /> <Post />
+    </div>
+  );
+};
+
+export default Posts;
